b14a053032f67ab68d20f338ac6c3f34f3790275
Hannah Justen <hjusten@bio.tamu.edu>
2024-04-22 00:20:34 -0500

    Update

--- genotype_pipeline/7a.fst.sh
+++ genotype_pipeline/7a.fst.sh
@@ -14,41 +14,26 @@
 
 module load GCC/9.3.0 iccifort/2019.5.281 VCFtools/0.1.16
 
-#vcftools \
-#--vcf genotype_all.drew.indel5bp.info.dp.biallelic.missing.recode.vcf \
-#--weir-fst-pop coastal_ids.txt \
-#--weir-fst-pop inland_ids.txt \
-#--chr super_scaffold_1 \
-#--out scaf_1_coastal_inland
-
-
-#vcftools \
-#--vcf genotype_all.drew.indel5bp.info.dp.biallelic.missing.recode.vcf \
-#--weir-fst-pop coastal_ids.txt \
-#--weir-fst-pop inland_ids.txt \
-#--chr scaffold_4_arrow_ctg1 \
-#--out scaf_4_coastal_inland
-
 vcftools \
 --vcf genotype_all.drew.indel5bp.info.dp.biallelic.missing.recode.vcf \
 --weir-fst-pop coastal_ids.txt \
 --weir-fst-pop inland_ids.txt \
---fst-window-size 10000 \
---out genome-wide_subspecies_fst_10kb
+--fst-window-size 25000 \
+--out genome-wide_subspecies_fst_25kb
 
 vcftools \
 --vcf genotype_all.drew.indel5bp.info.dp.biallelic.missing.recode.vcf \
 --weir-fst-pop pacific_spirit.txt \
 --weir-fst-pop porpoise.txt \
---fst-window-size 10000 \
---out genome-wide_coastal_fst_10kb
+--fst-window-size 25000 \
+--out genome-wide_coastal_fst_25kb
 
 vcftools \
 --vcf genotype_all.drew.indel5bp.info.dp.biallelic.missing.recode.vcf \
 --weir-fst-pop kamloops.txt \
 --weir-fst-pop kelowna.txt \
---fst-window-size 10000 \
---out genome-wide_inland_fst_10kb
+--fst-window-size 25000 \
+--out genome-wide_inland_fst_25kb
 
 
 
